f6f25275e10c82bc4982e60885bcb90a9e6801cd
Shazeeda <shazeedamohamed@gmail.com>
2025-03-15 13:20:59 -0700

    changes to server

--- server/db.js
+++ server/db.js
@@ -1,12 +1,8 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT || 5432, 
+  connectionString: process.env.DB_CONNECTION,
 });
 
 module.exports = pool;
