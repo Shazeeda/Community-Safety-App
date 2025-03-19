0da9b747d3dc3b08d3247e81f031439eb25e1601
Shazeeda <shazeedamohamed@gmail.com>
2025-03-19 15:30:52 -0700

    Fixed App.js import path

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "../App";
+import App from "./App";
+
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
